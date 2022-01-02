a7d087bc86d7b8565b7c41b1708fb5253c0f8185
Carl Kaaregaard <carl@carl.cc>
2022-01-02 22:05:07 +0000

    Add project files.

---
+++ CSharperLib.Tests/Extensions/StringExtensions.cs
@@ -0,0 +1,47 @@
+﻿using CSharperLib.Extensions;
+using Xunit;
+
+namespace CSharperLib.Tests.Extensions
+{
+    public class StringExtensions
+    {
+        [Theory]
+        [InlineData(null, true)]
+        [InlineData("", true)]
+        [InlineData(" ", false)]
+        [InlineData("\t", false)]
+        [InlineData("\r", false)]
+        [InlineData("\n", false)]
+        [InlineData("\r\n", false)]
+        [InlineData("ABC", false)]
+        public void IsNullOrEmpty(string value, bool expected)
+        {
+            // Act
+            bool result = value.IsNullOrEmpty();
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+
+        // --- IsNullOrWhiteSpace ---
+
+        [Theory]
+        [InlineData(null, true)]
+        [InlineData("", true)]
+        [InlineData(" ", true)]
+        [InlineData("\t", true)]
+        [InlineData("\r", true)]
+        [InlineData("\n", true)]
+        [InlineData("\r\n", true)]
+        [InlineData("ABC", false)]
+        public void IsNullOrWhiteSpace(string value, bool expected)
+        {
+            // Act
+            bool result = value.IsNullOrWhiteSpace();
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ CSharperLib/Extensions/StringExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+
+namespace CSharperLib.Extensions;
+
+[DebuggerStepThrough]
+public static class StringExtensions
+{
+    /// <summary>
+    /// Indicates whether the specified string is null or an empty string ("").
+    /// </summary>
+    /// <param name="value">The string to test.</param>
+    /// <returns>true if the value parameter is null or an empty string (""); otherwise, false.</returns>
+    public static bool IsNullOrEmpty(this string? value)
+    {
+        return string.IsNullOrEmpty(value);
+    }
+
+    /// <summary>
+    /// Indicates whether a specified string is null, empty, or consists only of white-space characters.
+    /// </summary>
+    /// <param name="value">The string to test.</param>
+    /// <returns>true if the value parameter is null or System.String.Empty, or if value consists exclusively of white-space characters.</returns>
+    public static bool IsNullOrWhiteSpace(this string? value)
+    {
+        return string.IsNullOrWhiteSpace(value);
+    }
+}
