e3d1f1275fba5c23294a4dd715c071a215bcbf4b
Carl Kaaregaard <carl@carl.cc>
2022-01-03 17:04:28 +0000

    Add IEnumerable extensions
    - Add None

---
+++ CSharperLib.Tests/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using CSharperLib.Extensions;
+using Xunit;
+
+namespace CSharperLib.Tests.Extensions;
+
+public class IEnumerableExtensions
+{
+    // --- None ---
+
+    [Theory]
+    [InlineData(new object[] { new string[] { }, true })]
+    [InlineData(new object[] { new string[] { "ABC" }, false })]
+    public void None(string[] source, bool expecteed)
+    {
+        // Act
+        bool result = source.None();
+
+        // Assert
+        Assert.Equal(expecteed, result);
+    }
+
+    public static readonly Func<string, bool> nonePredicate = (string value) => value != "";
+
+    public static readonly object[][] NoneWithPredicateDate = {
+        new object[] { new string[] { }, nonePredicate, true },
+        new object[] { new string[] { "" }, nonePredicate, true },
+        new object[] { new string[] { "", "ABC" }, nonePredicate, false }
+    };
+
+    [Theory, MemberData(nameof(NoneWithPredicateDate))]
+    public void NoneWithPredicate(string[] source, Func<string, bool> predicate, bool expecteed)
+    {
+        // Act
+        bool result = source.None(predicate);
+
+        // Assert
+        Assert.Equal(expecteed, result);
+    }
+}
---
+++ CSharperLib/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,30 @@
+﻿namespace CSharperLib.Extensions;
+
+/// <summary>
+/// IEnumerable Extensions
+/// </summary>
+public static class IEnumerableExtensions
+{
+    /// <summary>
+    /// Determines whether a sequence is empty.
+    /// </summary>
+    /// <typeparam name="TSource">The type of the elements of source.</typeparam>
+    /// <param name="source">The <see cref="IEnumerable{TSource}"/> System.Collections.Generic.IEnumerable`1 to check for emptiness.</param>
+    /// <returns><see langword="true"/> if the source sequence is empty; otherwise, <see langword="false"/>.</returns>
+    public static bool None<TSource>(this IEnumerable<TSource> source)
+    {
+        return !source.Any();
+    }
+
+    /// <summary>
+    /// Determines whether a sequence is empty or no elements of a sequence satisfies a condition.
+    /// </summary>
+    /// <typeparam name="TSource">The type of the elements of source.</typeparam>
+    /// <param name="source"></param>
+    /// <param name="predicate"></param>
+    /// <returns><see langword="true"/> if the source sequence is empty or no elements passes the test in the specified predicate; otherwise, <see langword="false"/>.</returns>
+    public static bool None<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+    {
+        return !source.Any(predicate);
+    }
+}
